174973a646534f887fdf08dfa66f2c8477bfd720
vietpv <vietpv@vnpost.vn>
2022-02-24 11:02:06 +0700

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
+    @RequestMapping("/home")
     @ResponseBody
     String home() {
         return "Hello World!";
